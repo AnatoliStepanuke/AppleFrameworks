d5e6b415f3af993ed6f5da79026a0f3e80b37183
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-02-09 19:29:51 +0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Anatoli on 09.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
