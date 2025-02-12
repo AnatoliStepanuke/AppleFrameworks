b9378762e2a09073cf1df10fa01cd9e70018d629
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-02-12 21:46:03 +0300

    Added grids

--- AppleFrameworks/View/FrameworkGridView.swift
+++ AppleFrameworks/View/FrameworkGridView.swift
@@ -1,133 +1,17 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
-    let columns: [GridItem] = [GridItem(.flexible())]
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
     
     var body: some View {
-        FrameworkTitleView(imageName: "app-clip", name: "App Clips")
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(imageName: "app-clip", name: "App Clips")
+            FrameworkTitleView(imageName: "arkit", name: "ARKit")
+            FrameworkTitleView(imageName: "swiftui", name: "SwiftUI")
+        }
     }
-    
-//    var body: some View {
-//        ZStack {
-//            VStack(spacing: 25) {
-//                HStack(spacing: 25) {
-//                    VStack {
-//                        Image("app-clip")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("App Clips")
-//                    }
-//                    
-//                    VStack {
-//                        Image("arkit")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("ARKit")
-//                    }
-//                    
-//                    VStack {
-//                        Image("carplay")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("CarPlay")
-//                    }
-//                }
-//                
-//                HStack(spacing: 25) {
-//                    VStack {
-//                        Image("catalyst")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("Catalyst")
-//                    }
-//                    
-//                    VStack {
-//                        Image("classkit")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("ClassKit")
-//                    }
-//                    
-//                    VStack {
-//                        Image("cloudkit")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("CloudKit")
-//                    }
-//                }
-//                
-//                HStack(spacing: 25) {
-//                    VStack {
-//                        Image("coreml")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("CoreML")
-//                    }
-//                    
-//                    VStack {
-//                        Image("healthkit")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("HealthKit")
-//                    }
-//                    
-//                    VStack {
-//                        Image("metal")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("Metal")
-//                    }
-//                }
-//                
-//                HStack(spacing: 25) {
-//                    VStack {
-//                        Image("sf-symbols")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("SF Symbols")
-//                    }
-//                    
-//                    VStack {
-//                        Image("sirikit")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("SiriKit")
-//                    }
-//                    
-//                    VStack {
-//                        Image("spritekit")
-//                            .renderingMode(.original)
-//                            .resizable()
-//                            .aspectRatio(contentMode: .fit)
-//                            .frame(width: 100, height: 100)
-//                        Text("SpriteKit")
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
 
 #Preview {
@@ -149,7 +33,6 @@ struct FrameworkTitleView: View {
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
-
         }
     }
 }
