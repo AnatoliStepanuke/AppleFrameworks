4e77a284bed7156faa1c126844bea14659dac5ff
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2025-03-03 20:26:31 +0300

    Files refactoring

---
+++ AppleFrameworks/Custom/Buttons/DismissButton.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct DismissButton: View {
+    
+    @Binding var isShowingDetailView: Bool
+
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            Button {
+                isShowingDetailView = false
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(Color(.label))
+                    .imageScale(.large)
+                    .frame(width: 44, height: 44)
+            }
+        }.padding()
+    }
+}
+
+#Preview {
+    DismissButton(isShowingDetailView: .constant(false))
+}
